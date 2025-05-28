06116a0955ccfd6636cdbb7020778e30bac0c181
Max182xx <m.garciapro34@gmail.com>
2025-05-28 17:07:00 +0200

    Modification de la page Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,74 +1,24 @@
 import Title from "./Title";
-import img1 from "../assets/projects/1.png";
-import img2 from "../assets/projects/2.png";
-import img3 from "../assets/projects/3.png";
-import img4 from "../assets/projects/4.png";
-import img5 from "../assets/projects/5.png";
-import img6 from "../assets/projects/6.png";
-import { Github, Video } from "lucide-react";
+import img1 from "../assets/img2.jpg";
+import { Github } from "lucide-react";
 
 const projects = [
   {
     id: 1,
-    title: "Gestionnaire de tâches",
+    title: "Gestionnaire de stock",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae magni deserunt debitis recusandae ab harum totam, eum facilis et ratione officia ut inventore aspernatur",
-    technologies: ["React", "Node.js", "Tailwind CSS"],
-    demoLink: "#",
-    repoLink: "#",
+      "Je développe actuellement une application de gestion de stock en utilisant React, Next.js et Prisma pour la gestion de la base de données. " +
+      "Ce projet full-stack me permet de mettre en œuvre mes compétences à la fois en front-end (structuration des composants, gestion des états, routing, etc.) " +
+      "et en back-end (modélisation de la base de données, requêtes SQL via Prisma, logique serveur avec les API routes de Next.js). " +
+      "L'objectif est de permettre un suivi simple et efficace des stocks d’articles, avec des fonctionnalités comme l’ajout, la modification et la suppression de produits, " +
+      "ainsi que l’affichage en temps réel des quantités disponibles. Vous pouvez suivre l’évolution de mon travail et consulter le code source sur GitHub via le lien ci-dessous.",
+    technologies: ["React", "Next.js", "Prisma"],
+    demoLink: "",
+    repoLink: "https://github.com/Max182xx/asso-stock",
     image: img1,
   },
-  {
-    id: 2,
-    title: "Plateforme E-commerce",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae magni deserunt debitis recusandae ab harum totam, eum facilis et ratione officia ut inventore aspernatur",
-    technologies: ["Next.js", "TypeScript", "Prisma"],
-    demoLink: "#",
-    repoLink: "#",
-    image: img2,
-  },
-  {
-    id: 3,
-    title: "Portfolio interactif",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae magni deserunt debitis recusandae ab harum totam, eum facilis et ratione officia ut inventore aspernatur",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    demoLink: "#",
-    repoLink: "#",
-    image: img3,
-  },
-  {
-    id: 4,
-    title: "Application de Chat en temps réel",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae magni deserunt debitis recusandae ab harum totam, eum facilis et ratione officia ut inventore aspernatur",
-    technologies: ["React", "Socket.io", "Express.js"],
-    demoLink: "#",
-    repoLink: "#",
-    image: img4,
-  },
-  {
-    id: 5,
-    title: "Système de réservation de salles",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae magni deserunt debitis recusandae ab harum totam, eum facilis et ratione officia ut inventore aspernatur",
-    technologies: ["Next.js", "MongoDB", "Chakra UI"],
-    demoLink: "#",
-    repoLink: "#",
-    image: img5,
-  },
-  {
-    id: 6,
-    title: "Analyseur de sentiment",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae magni deserunt debitis recusandae ab harum totam, eum facilis et ratione officia ut inventore aspernatur",
-    technologies: ["Python", "Flask", "NLTK"],
-    demoLink: "#",
-    repoLink: "#",
-    image: img6,
-  },
 ];
+
 
 const Projects = () => {
   return (
@@ -95,14 +45,14 @@ const Projects = () => {
               ))}
             </div>
             <div className="flex ">
-                <a className="btn btn-accent w-2/3" href={project.demoLink}>
-                Demo
-              <Video className="w-4" />
+              <a
+                className="btn btn-neutral w-full ml-2"
+                href={project.repoLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github className="w-4" />
               </a>
-              <a className="btn btn-neutral w-1/3 ml-2" href={project.demoLink}>
-              <Github className="w-4" />
-              </a>
-              
             </div>
           </div>
         ))}
